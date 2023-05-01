5f62bb174a9e8f4e3e0c4fc6ee41a57b205cbc0d
parkchangmoon <76508939+parkchangmoon@users.noreply.github.com>
2023-05-01 12:04:10 +0900

    update yogaSutra.js

--- yogaSutra.js
+++ yogaSutra.js
@@ -1,4 +1,6 @@
 const yogaSutra = [
+	1,2,3,4,5,
+	/*
 ['ys1- 1 공부선언',
 '지금 요가 공부!',
 'अथ योगानुशासनम् ॥१॥',
@@ -3050,5 +3052,5 @@ const yogaSutraBooks = [
 'Jayadeva Yogendra, Yoga Sutra of Patanjali, Zen Publications, 2009',
 
 '윤홍식, <요가수트라 강의> pdf파일. 인터넷 공개자료. 여기에는 요가경 전체 번역과 간단한 설명이 첨부되어 있음',
-
+*/
 ];
